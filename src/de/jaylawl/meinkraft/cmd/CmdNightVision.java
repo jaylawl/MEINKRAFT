0ac3034ebc5bdf147811cb9cb0a3a102f2f64c48
jaylawl <jaylawl@mail.de>
2021-01-24 14:27:17 +0100

    - night vision command now remembers the potential night vision effects players had prior - fixed a small permission error

--- src/de/jaylawl/meinkraft/cmd/CmdNightVision.java
+++ src/de/jaylawl/meinkraft/cmd/CmdNightVision.java
@@ -11,7 +11,27 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class CmdNightVision implements CommandExecutor {
+
+    private final static short CUSTOM_EFFECT_IDENTIFIER = 214;
+    private final static PotionEffect PERMANENT_NIGHT_VISION = new PotionEffect(
+            PotionEffectType.NIGHT_VISION,
+            2147483647,
+            CUSTOM_EFFECT_IDENTIFIER,
+            true,
+            false,
+            true
+    );
+
+    private final ConcurrentHashMap<UUID, PotionEffect> priorNightVisionEffects = new ConcurrentHashMap<>();
+
+    public CmdNightVision() {
+    }
+
+    //
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
@@ -22,7 +42,6 @@ public class CmdNightVision implements CommandExecutor {
         }
 
         Player affectedPlayer;
-        boolean senderEqualsAffected = false;
 
         if (args.length == 0) {
             if (sender instanceof Player) {
@@ -39,33 +58,44 @@ public class CmdNightVision implements CommandExecutor {
             }
         }
 
-        if (sender == affectedPlayer) {
-            senderEqualsAffected = true;
+        boolean senderEqualsAffected = sender == affectedPlayer;
+        if (!senderEqualsAffected) {
             if (!CmdPermission.hasOthers(sender, label)) {
                 MessagingUtil.noPermissionOthers(sender);
                 return true;
             }
         }
 
-        boolean grant = true;
+        boolean hasPermanentNightVision = false;
         if (affectedPlayer.hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
-            grant = affectedPlayer.getPotionEffect(PotionEffectType.NIGHT_VISION).getAmplifier() != 214;
+            PotionEffect priorNightVisionEffect = affectedPlayer.getPotionEffect(PotionEffectType.NIGHT_VISION);
+            if (priorNightVisionEffect != null) {
+                if (priorNightVisionEffect.getAmplifier() == CUSTOM_EFFECT_IDENTIFIER) {
+                    hasPermanentNightVision = true;
+                } else {
+                    this.priorNightVisionEffects.put(affectedPlayer.getUniqueId(), priorNightVisionEffect);
+                }
+            }
         }
 
-        if (grant) {
-            affectedPlayer.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 2147483647, 214, true, false, true), true);
+        if (hasPermanentNightVision) {
+            affectedPlayer.removePotionEffect(PotionEffectType.NIGHT_VISION);
+            UUID affectedPlayerUniqueId = affectedPlayer.getUniqueId();
+            PotionEffect priorNightVisionEffect = this.priorNightVisionEffects.get(affectedPlayerUniqueId);
+            if (priorNightVisionEffect != null) {
+                affectedPlayer.addPotionEffect(priorNightVisionEffect);
+                this.priorNightVisionEffects.remove(affectedPlayerUniqueId);
+            }
+            sender.sendMessage("Removed \"permanent\" night vision from " + affectedPlayer.getName());
+            if (!senderEqualsAffected) {
+                MessagingUtil.notifyPlayer(affectedPlayer, "A wizard has cast away your permanent night vision ability");
+            }
+        } else {
+            affectedPlayer.addPotionEffect(PERMANENT_NIGHT_VISION);
             sender.sendMessage("Gave \"permanent\" night vision to " + affectedPlayer.getName());
             if (!senderEqualsAffected) {
                 MessagingUtil.notifyPlayer(affectedPlayer, "A wizard made you drink a cauldron of night vision potion");
             }
-
-        } else {
-            affectedPlayer.removePotionEffect(PotionEffectType.NIGHT_VISION);
-            sender.sendMessage("Removed \"permanent\" night vision from " + affectedPlayer.getName());
-            if (!senderEqualsAffected) {
-                MessagingUtil.notifyPlayer(affectedPlayer, "A wizard has cast away your night vision ability");
-            }
-
         }
 
         return true;
